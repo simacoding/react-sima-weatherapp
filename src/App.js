59016f643e07bccd7ef65ed8c46a8267cee74959
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-03 10:43:20 +0200

    added bootstrap
    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Simone Ma{" "}
+          <a
+            href="https://github.com/simacoding/react-sima-weatherapp"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
