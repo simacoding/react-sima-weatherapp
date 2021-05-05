2dff52a5975fabbc686df6a1d3598f4c3232d622
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-05 08:15:34 +0200

    First try to integrate api weather
    installed axios, imported axios to my project and wrote the code in Weather.js. (but something is still wrong)

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,82 @@
-import React from "react";
+import React, { useStates } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="Search"
-              placeholder="Enter city..."
-              className="form-control"
-              autoFocus="on" //marks the form so you can see it directly
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="Submit"
-              value="Search"
-              className="btn btn-primary w-100" //defined button from bootstrap //w-100 = 100% width
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Munich</h1>
-      <ul>
-        <li>Monday 11:00 am</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-              className="float-left"
-            />
-            <div className="float-left">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
+  let [weatherData, setWeatherData] = useStates({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setWeatherData({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      date: "Monday 07:00",
+      ready: true,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="Search"
+                placeholder="Enter city..."
+                className="form-control"
+                autoFocus="on" //marks the form so you can see it directly
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="Submit"
+                value="Search"
+                className="btn btn-primary w-100" //defined button from bootstrap //w-100 = 100% width
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13 km/h</li>
-          </ul>
+        </form>
+        <h1>Munich</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "641aea60f164eb7376c34eaed6daea65";
+    let city = "Munich";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
