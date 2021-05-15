acf7bd3359b2efd0b1ae262e47bd04fe87f301c6
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-15 11:08:20 +0200

    Substitute code with a new function
    to make the code more readable, I subtitute the code from the else-statement with a new function "load()", which I defined before the if-else-statement.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,14 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "641aea60f164eb7376c34eaed6daea65";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -35,11 +43,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "641aea60f164eb7376c34eaed6daea65";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
