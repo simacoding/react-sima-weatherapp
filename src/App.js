817d07cbac0915ef8c27d1f1fd9479211abd041c
simacoding <77894138+simacoding@users.noreply.github.com>
2021-05-15 11:02:21 +0200

    Fixed some warnings from the console
    Add a return in WeatherForecast, because we forgot it. It was ok (not an error), but it was a warning.
    
    Add in App.js to the link the rel="noreferrer", because the console gave us a warning, that it's missing.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
           <a
             href="https://github.com/simacoding/react-sima-weatherapp"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced
           </a>
